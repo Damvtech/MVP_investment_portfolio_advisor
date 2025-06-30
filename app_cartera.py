5dae0e7a70f41156fceafc02ea5211584d8546a1
Damvtech <damovi.digital@gmail.com>
2025-06-30 22:32:34 +0200

    60% oppacity widget background

--- app_cartera.py
+++ app_cartera.py
@@ -33,7 +33,7 @@ st.markdown(
     }}
 
     [data-testid="stVerticalBlock"] {{
-        background-color: rgba(0, 0, 0, 0.5);  /* fondo negro con 50% de opacidad */
+        background-color: rgba(0, 0, 0, 0.6);  /* fondo negro con 60% de opacidad */
         padding: 2rem;
         border-radius: 12px;
     }}
